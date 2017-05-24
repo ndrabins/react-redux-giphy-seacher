2d1724410902c6c95f6199086002094079d24f89
ndrabins <noah.drabinsky1@gmail.com>
2017-05-24 14:36:16 -0400

    Added firebase auth, favoriting

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,67 @@
 import request from 'superagent';
 import { browserHistory } from 'react-router';
+import Firebase from 'firebase';
 
 export const OPEN_MODAL = 'OPEN_MODAL';
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 export const REQUEST_GIFS = 'REQUEST_GIFS';
-export const SIGN_IN_USER = 'SIGN_IN_USER';
 export const SIGN_OUT_USER = 'SIGN_OUT_USER';
+export const AUTH_ERROR = 'AUTH_ERROR';
+export const AUTH_USER = 'AUTH_USER';
+export const FETCH_FAVORITED_GIFS = 'FETCH_FAVORITED_GIFS';
 
 const API_URL = 'http://api.giphy.com/v1/gifs/search?q=';
 const API_KEY = '&api_key=dc6zaTOxFJmzC';
 
-export function requestGifs(term = null) {
-  const data = request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`);
+const config = {
+  apiKey: "AIzaSyCOQjodGigZrGIm7fTmgg7Q3Ac3UvNhNfQ",
+  authDomain: "react-redux-gif.firebaseapp.com",
+  databaseURL: "https://react-redux-gif.firebaseio.com",
+  projectId: "react-redux-gif",
+  storageBucket: "react-redux-gif.appspot.com",
+  messagingSenderId: "456475713820"
+};
 
-  return {
-    type: REQUEST_GIFS,
-    payload: data
+Firebase.initializeApp(config);
+
+export function requestGifs(term = null) {
+  return function(dispatch) {
+    request.get(`${API_URL}${term.replace(/\s/g, '+')}${API_KEY}`).then(response => {
+      dispatch({
+        type: REQUEST_GIFS,
+        payload: response
+      });
+    });
+  }
+}
+
+export function favoriteGif({selectedGif}) {
+  const userUid = Firebase.auth().currentUser.uid;
+  const gifId = selectedGif.id;
+
+  return dispatch => Firebase.database().ref(userUid).update({
+    [gifId]: selectedGif
+  });
+}
+
+export function unfavoriteGif({selectedGif}) {
+  const userUid = Firebase.auth().currentUser.uid;
+  const gifId = selectedGif.id;
+
+  return dispatch => Firebase.database().ref(userUid).child(gifId).remove();
+}
+
+
+export function fetchFavoritedGifs() {
+  return function(dispatch) {
+    const userUid = Firebase.auth().currentUser.uid;
+
+    Firebase.database().ref(userUid).on('value', snapshot => {
+      dispatch({
+        type: FETCH_FAVORITED_GIFS,
+        payload: snapshot.val()
+      })
+    });
   }
 }
 
@@ -32,20 +78,62 @@ export function closeModal() {
   }
 }
 
-export function signInUser()
-{
-  browserHistory.push('/favorites');
-
-  return {
-    type: SIGN_IN_USER
+export function signUpUser(credentials) {
+  return function(dispatch) {
+    Firebase.auth().createUserWithEmailAndPassword(credentials.email, credentials.password)
+      .then(response => {
+        dispatch(authUser());
+        browserHistory.push('/favorites');
+      })
+      .catch(error => {
+        console.log(error);
+        dispatch(authError(error));
+      });
   }
 }
 
-export function signOutUser()
-{
+export function signInUser(credentials) {
+  return function(dispatch) {
+    Firebase.auth().signInWithEmailAndPassword(credentials.email, credentials.password)
+      .then(response => {
+        dispatch(authUser());
+        browserHistory.push('/favorites');
+      })
+      .catch(error => {
+        dispatch(authError(error));
+      });
+  }
+}
+
+export function signOutUser() {
   browserHistory.push('/');
-  
+
   return {
     type: SIGN_OUT_USER
+  }
+}
+
+export function verifyAuth() {
+  return function (dispatch) {
+    Firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        dispatch(authUser());
+      } else {
+        dispatch(signOutUser());
+      }
+    });
+  }
+}
+
+export function authUser() {
+  return {
+    type: AUTH_USER
+  }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
   }
 }
